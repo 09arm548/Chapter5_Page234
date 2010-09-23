76d852aada185cb4c236e29ebaed0edb50c0136f
AndreaMcPherson <arm20@sage.southhills.edu>
2010-09-23 16:21:40 -0400

    finished data.txt working on getting first name separated

--- frmMain.vb
+++ frmMain.vb
@@ -1,4 +1,25 @@
 ﻿Public Class frmMain
 
 
+    Private Sub btnProcess_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnProcess.Click
+
+        Dim strInvoiceNumber As String = ("Invoice Number:")
+        Dim strfName As String
+        Dim strlName As String
+        Dim strAddress As String = txtAddress.Text
+        Dim strCity As String = txtCity.Text
+        Dim intChair As Integer = CInt(txtNumChair.Text)
+        Dim intSofa As Integer = CInt(txtNumSofa.Text)
+        Dim dblTax As Double = 0.0
+        Dim dblTotal As Double = 0.0
+        Dim strFullName As String = txtName.Text
+
+        strfName = CStr(strFullName.IndexOf(","))
+
+        lstInvoice.Items.Add = (strfName)
+
+
+
+
+    End Sub
 End Class
