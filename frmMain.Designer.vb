91cb4eb084b56ad4c2d334ff5f2317baee784967
AndreaMcPherson <arm20@sage.southhills.edu>
2010-09-23 15:57:12 -0400

    GUI built next write data file

--- frmMain.Designer.vb
+++ frmMain.Designer.vb
@@ -22,53 +22,175 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btnProcess = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.txtCity = New System.Windows.Forms.TextBox()
+        Me.txtNumChair = New System.Windows.Forms.TextBox()
+        Me.txtNumSofa = New System.Windows.Forms.TextBox()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblAddress = New System.Windows.Forms.Label()
+        Me.lblCity = New System.Windows.Forms.Label()
+        Me.lblNumChairs = New System.Windows.Forms.Label()
+        Me.lblNumSofas = New System.Windows.Forms.Label()
+        Me.lstInvoice = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnProcess
         '
-        Me.Button1.Location = New System.Drawing.Point(92, 275)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnProcess.Location = New System.Drawing.Point(92, 275)
+        Me.btnProcess.Name = "btnProcess"
+        Me.btnProcess.Size = New System.Drawing.Size(75, 23)
+        Me.btnProcess.TabIndex = 0
+        Me.btnProcess.Text = "Process Orders"
+        Me.btnProcess.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnClear
         '
-        Me.Button2.Location = New System.Drawing.Point(395, 274)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 1
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnClear.Location = New System.Drawing.Point(395, 274)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear Order Form"
+        Me.btnClear.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnQuit
         '
-        Me.Button3.Location = New System.Drawing.Point(722, 274)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnQuit.Location = New System.Drawing.Point(722, 274)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuit.TabIndex = 2
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(207, 22)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 3
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(207, 78)
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(100, 20)
+        Me.txtAddress.TabIndex = 4
+        '
+        'txtCity
+        '
+        Me.txtCity.Location = New System.Drawing.Point(207, 137)
+        Me.txtCity.Name = "txtCity"
+        Me.txtCity.Size = New System.Drawing.Size(100, 20)
+        Me.txtCity.TabIndex = 5
+        '
+        'txtNumChair
+        '
+        Me.txtNumChair.Location = New System.Drawing.Point(207, 196)
+        Me.txtNumChair.Name = "txtNumChair"
+        Me.txtNumChair.Size = New System.Drawing.Size(39, 20)
+        Me.txtNumChair.TabIndex = 6
+        '
+        'txtNumSofa
+        '
+        Me.txtNumSofa.Location = New System.Drawing.Point(207, 234)
+        Me.txtNumSofa.Name = "txtNumSofa"
+        Me.txtNumSofa.Size = New System.Drawing.Size(39, 20)
+        Me.txtNumSofa.TabIndex = 7
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(4, 23)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(85, 26)
+        Me.lblName.TabIndex = 8
+        Me.lblName.Text = "Customer Name:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(Last, First)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'lblAddress
+        '
+        Me.lblAddress.AutoSize = True
+        Me.lblAddress.Location = New System.Drawing.Point(7, 85)
+        Me.lblAddress.Name = "lblAddress"
+        Me.lblAddress.Size = New System.Drawing.Size(48, 13)
+        Me.lblAddress.TabIndex = 10
+        Me.lblAddress.Text = "Address:"
+        '
+        'lblCity
+        '
+        Me.lblCity.AutoSize = True
+        Me.lblCity.Location = New System.Drawing.Point(7, 144)
+        Me.lblCity.Name = "lblCity"
+        Me.lblCity.Size = New System.Drawing.Size(79, 13)
+        Me.lblCity.TabIndex = 11
+        Me.lblCity.Text = "City, State, Zip:"
+        '
+        'lblNumChairs
+        '
+        Me.lblNumChairs.AutoSize = True
+        Me.lblNumChairs.Location = New System.Drawing.Point(7, 199)
+        Me.lblNumChairs.Name = "lblNumChairs"
+        Me.lblNumChairs.Size = New System.Drawing.Size(77, 26)
+        Me.lblNumChairs.TabIndex = 12
+        Me.lblNumChairs.Text = "Number of" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "chairs ordered:"
+        '
+        'lblNumSofas
+        '
+        Me.lblNumSofas.AutoSize = True
+        Me.lblNumSofas.Location = New System.Drawing.Point(7, 241)
+        Me.lblNumSofas.Name = "lblNumSofas"
+        Me.lblNumSofas.Size = New System.Drawing.Size(74, 26)
+        Me.lblNumSofas.TabIndex = 13
+        Me.lblNumSofas.Text = "Number of " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "sofas ordered:"
+        '
+        'lstInvoice
+        '
+        Me.lstInvoice.FormattingEnabled = True
+        Me.lstInvoice.Location = New System.Drawing.Point(392, 25)
+        Me.lstInvoice.Name = "lstInvoice"
+        Me.lstInvoice.Size = New System.Drawing.Size(404, 225)
+        Me.lstInvoice.TabIndex = 14
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(932, 334)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lstInvoice)
+        Me.Controls.Add(Me.lblNumSofas)
+        Me.Controls.Add(Me.lblNumChairs)
+        Me.Controls.Add(Me.lblCity)
+        Me.Controls.Add(Me.lblAddress)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.txtNumSofa)
+        Me.Controls.Add(Me.txtNumChair)
+        Me.Controls.Add(Me.txtCity)
+        Me.Controls.Add(Me.txtAddress)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnProcess)
         Me.Name = "frmMain"
-        Me.Text = "Form1"
+        Me.Text = "Furniture Order Form"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents btnProcess As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnQuit As System.Windows.Forms.Button
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtAddress As System.Windows.Forms.TextBox
+    Friend WithEvents txtCity As System.Windows.Forms.TextBox
+    Friend WithEvents txtNumChair As System.Windows.Forms.TextBox
+    Friend WithEvents txtNumSofa As System.Windows.Forms.TextBox
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lblAddress As System.Windows.Forms.Label
+    Friend WithEvents lblCity As System.Windows.Forms.Label
+    Friend WithEvents lblNumChairs As System.Windows.Forms.Label
+    Friend WithEvents lblNumSofas As System.Windows.Forms.Label
+    Friend WithEvents lstInvoice As System.Windows.Forms.ListBox
 
 End Class
